9d53888699dfa5c092dfde33b090abcceca6aa42
mikiyawv <mikiyawv@gmail.com>
2022-10-12 16:25:44 -0700

    Fully completed exercise

---
+++ javascript.js
@@ -0,0 +1,22 @@
+        const container = document.querySelector('.container');
+
+        const p = document.createElement('p');
+        const h3 = document.createElement('h3');
+        const div2 = document.createElement('div');
+        const h1 = document.createElement('h1');
+        const p2 = document.createElement('p');
+
+        p.classList.add('red');
+        h3.setAttribute('class', 'blue');
+        div2.classList.add('blackPink');
+
+        p.textContent = "Hey I'm red!";
+        h3.textContent = "I'm a blue h3!";
+        h1.textContent = "I'm in a div";
+        p2.textContent = "ME TOO!";
+
+        container.appendChild(p);
+        container.appendChild(h3);
+        div2.appendChild(h1);
+        div2.appendChild(p2);
+        container.appendChild(div2);
